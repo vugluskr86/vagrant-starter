456ba8a94be354f766be34bec19c9e11049a1e18
vugluskr <vugluskr@onalone.com>
2016-02-05 12:35:55 +0500

    add generate android arm toolchain

--- provision/android_toolchain.sh
+++ provision/android_toolchain.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Installing c++ toolchain"
+echo "Installing android toolchain"
 
 sudo dpkg --add-architecture i386
 sudo apt-get update
@@ -9,15 +9,15 @@ sudo apt-get install -y ia32-libs
 sudo apt-get install -y libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5 lib32z1
 sudo apt-get install -y python-software-properties
 
-mkdir -p ~/android
-rm -rf ~/android
-mkdir -p ~/android
+mkdir -p /opt/android
+rm -rf /opt/android
+mkdir -p /opt/android
 
 echo "Installing Android SDK..."
 cd ~
 wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz >/dev/null 2>&1
 tar -zxvf android-sdk_r24.4.1-linux.tgz
-mv ~/android-sdk-linux ~/android
+mv ~/android-sdk-linux /opt/android
 rm -rf ~/android-sdk-linux
 rm -rf ~/android-sdk_r24.4.1-linux.tgz
 
@@ -26,15 +26,28 @@ cd ~
 wget http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin >/dev/null 2>&1
 chmod a+x android-ndk-r10e-linux-x86_64.bin
 ./android-ndk-r10e-linux-x86_64.bin
-mv ~/android-ndk-r10e ~/android
+mv ~/android-ndk-r10e /opt/android
 rm -rf ~/android-ndk-r10e
 rm -rf ~/android-ndk-r10e-linux-x86_64.bin
 
+chown -R vagrant:vagrant /opt/android
+
+su - vagrant
+
 echo "Updating ANDROID_HOME..."
-cd ~/
-echo "export ANDROID_HOME=\"~/android/android-sdk-linux\"" >> .profile
-echo "export ANDROID_NDK=\"~/android/android-ndk-r10e\"" >> .profile
+cd ~
+echo "export ANDROID_HOME=\"/opt/android/android-sdk-linux\"" >> .profile
+echo "export ANDROID_NDK=\"/opt/android/android-ndk-r10e\"" >> .profile
 echo "export PATH=\$ANDROID_NDK:\$ANDROID_HOME/tools:\$ANDROID_HOME/platform-tools:\$PATH" >> .profile
+
 source ~/.profile
 
 echo "yes" | android update sdk -u -a -t 33
+
+sudo mkdir -p /opt/android-toolchain-arm
+sudo chown -R vagrant:vagrant /opt/android-toolchain-arm
+/opt/android/android-ndk-r10e/build/tools/make-standalone-toolchain.sh --platform=android-16 --arch=arm --install-dir=/opt/android-toolchain-arm
+
+sudo mkdir -p /opt/android-toolchain-x86
+sudo chown -R vagrant:vagrant /opt/android-toolchain-x86
+/opt/android/android-ndk-r10e/build/tools/make-standalone-toolchain.sh --platform=android-16 --arch=x86 --install-dir=/opt/android-toolchain-x86
